48d50e71e34b632ea561445b98ab835379d6d4b5
Kyeongdoo Hur <1kdhur2@gmail.com>
2023-03-14 19:56:27 +0900

    init

---
+++ src/test/java/com/probrain/cnu_clubcommunity/CnuClubCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.probrain.cnu_clubcommunity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnuClubCommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
